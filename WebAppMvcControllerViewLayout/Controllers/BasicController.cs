551313865c8f99ac0f75d2ac35f14f2c475d85c0
daniel <danieljustino79@gmail.com>
2016-01-13 10:54:56 -0600

    basic-medium

---
+++ WebAppMvcControllerViewLayout/Controllers/BasicController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppMvcControllerViewLayout.Controllers
+{
+    public class ObjectComplex
+    {
+        public char character;
+        public string name;
+    }
+    public class BasicController : Controller
+    {
+        //
+        // GET: /Basic/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public void Test()
+        {
+
+        }
+
+        public ActionResult Helpers()
+        {
+            return View();
+        }
+
+        public ActionResult RequestQueryStringParam(int cod, string name)
+        {
+            ViewBag.cod = "RequestQueryStringParam: " + cod;
+            ViewBag.name = "RequestQueryStringParam: " + name;
+            return View();
+        }
+
+        public ActionResult RequestRouteParam(int id)
+        {
+            ViewBag.id = "RequestRouteParam: " + id;
+            return View();
+        }        
+
+        public ActionResult PassingData()
+        {
+            ViewBag.ViewBagParam1 = "text raw";
+            ViewData["ViewDataParam1"] = 1;
+
+            TempData["TempDataParam1"] = 1.1;
+            Session["SessionParam1"] = true;
+
+            ViewBag.ObjectComplex = new ObjectComplex{ character = 'a', name = "Sam" };
+
+            return View();
+        }
+
+        public ActionResult BundleSequence()
+        {
+            return View();
+        }
+
+        public string Calc(int v1, int v2)
+        {
+            return (v1 + v2).ToString();
+        }
+
+
+
+    }
+}
---
+++ WebAppMvcControllerViewLayout/Controllers/MediumController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppMvcControllerViewLayout.Controllers
+{
+    public class MediumController : Controller
+    {
+        //
+        // GET: /Medium/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DestinyRedirect()
+        {
+            return Redirect("http://localhost:34108/Medium/FinalDestinyRedirect");
+        }
+        public ActionResult FinalDestinyRedirect()
+        {
+            return View();
+        }
+
+        public ActionResult DestinyRedirectPermanent()
+        {
+            return RedirectPermanent("http://localhost:34108/Medium/FinalDestinyRedirectPermanent");
+        }
+        public ActionResult FinalDestinyRedirectPermanent()
+        {
+            return View();            
+        }
+
+        public ActionResult DestinyRedirectToAction()
+        {
+            return RedirectToAction("FinalDestinyRedirectToAction");
+        }
+        public ActionResult FinalDestinyRedirectToAction()
+        {
+            return View();            
+        }
+
+        public ActionResult DestinyRedirectToActionPermanent()
+        {
+            return RedirectToActionPermanent("FinalDestinyRedirectToActionPermanent");
+        }
+        public ActionResult FinalDestinyRedirectToActionPermanent()
+        {
+            return View();            
+        }
+
+        public ActionResult DestinyRedirectToRoute()
+        {
+            return RedirectToRoute(new { id = 1 });
+        }
+        public ActionResult DestinyRedirectToRoute(int id)
+        {
+            return View();            
+        }
+
+        public ActionResult DestinyRedirectToRoutePermanent()
+        {
+            return RedirectToRoutePermanent(new { id = 1 });
+        }
+        public ActionResult DestinyRedirectToRoutePermanent(int id)
+        {
+            return View();
+        }
+    }
+}
